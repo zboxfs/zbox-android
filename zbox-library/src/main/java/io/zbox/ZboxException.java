1e86d003f1bbf40171779f4e6452cd2cfbb8eb89
Bo Lu <lv.patrick@gmail.com>
2018-12-05 23:35:42 +1100

    add zbox transport

--- zbox-library/src/main/java/io/zbox/ZboxException.java
+++ zbox-library/src/main/java/io/zbox/ZboxException.java
@@ -1,7 +1,14 @@
 package io.zbox;
 
 public class ZboxException extends Exception {
-    public ZboxException(String message) {
-        super(message);
+
+    public ZboxException() { super(); }
+
+    public ZboxException(String message) { super(message); }
+
+    public ZboxException(String message, Throwable cause) { super(message, cause); }
+
+    public ZboxException(Throwable cause) {
+        super(cause);
     }
 }
